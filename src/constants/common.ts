e51e446876f958b1aec72808b3f47245e2800098
Rubén Cisneros <ruben.cns@gmail.com>
2025-02-16 23:34:39 +0100

    feat: show group 2 by default

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,32 +1,31 @@
-export const standingsCsvUrl = "https://datos.madrid.es/egob/catalogo/211549-1-juegos-deportivos-actual.csv"
-export const gamesCsvUrl = "https://datos.madrid.es/egob/catalogo/211549-3-juegos-deportivos-actual.csv"
+export const standingsCsvUrl =
+  "https://datos.madrid.es/egob/catalogo/211549-1-juegos-deportivos-actual.csv"
+export const gamesCsvUrl =
+  "https://datos.madrid.es/egob/catalogo/211549-3-juegos-deportivos-actual.csv"
 
-export const competitionCode = '8859'
-export const groupCode1 = '24203'
-export const groupCode2 = '24204'
+export const competitionCode = "8859"
+export const groupCode1 = "24203"
+export const groupCode2 = "24204"
 
-export const groupCodes: string[] = [
-  groupCode1,
-  groupCode2,
-]
+export const groupCodes: string[] = [groupCode1, groupCode2]
 
 export const courtInitials = "CDM"
 
-export const colors: { name: string, hex: string }[] = [
-  { name: "rojo", hex: "#e80101", },
-  { name: "roja", hex: "#e80101", },
-  { name: "azul", hex: "#0059ff", },
+export const colors: { name: string; hex: string }[] = [
+  { name: "rojo", hex: "#e80101" },
+  { name: "roja", hex: "#e80101" },
+  { name: "azul", hex: "#0059ff" },
   { name: "verde", hex: "#008c00" },
-  { name: "amarillo", hex: "#c5b436", },
-  { name: "amarilla", hex: "#c5b436", },
-  { name: "naranja", hex: "#ff7f00", },
-  { name: "morado", hex: "#ff00ff", },
-  { name: "morada", hex: "#ff00ff", },
-  { name: "gris", hex: "#878787", },
-  { name: "negro", hex: "#2e2e2e", },
-  { name: "negra", hex: "#2e2e2e", },
-  { name: "blanco", hex: "#878787", },
-  { name: "blanca", hex: "#878787", },
-  { name: "marrón", hex: "#8B4513", },
-  { name: "rosa", hex: "#FFC0CB", }];
-
+  { name: "amarillo", hex: "#c5b436" },
+  { name: "amarilla", hex: "#c5b436" },
+  { name: "naranja", hex: "#ff7f00" },
+  { name: "morado", hex: "#ff00ff" },
+  { name: "morada", hex: "#ff00ff" },
+  { name: "gris", hex: "#878787" },
+  { name: "negro", hex: "#2e2e2e" },
+  { name: "negra", hex: "#2e2e2e" },
+  { name: "blanco", hex: "#878787" },
+  { name: "blanca", hex: "#878787" },
+  { name: "marrón", hex: "#8B4513" },
+  { name: "rosa", hex: "#FFC0CB" },
+]
